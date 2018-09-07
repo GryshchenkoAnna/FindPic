fb000006d89c7cf5974c2de02bf8d9429c9615cc
Viktor Bulvarenko <viktor@bulvarenko.pw>
2018-09-07 12:57:49 +0300

    start coding FindPic

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,21 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    // some: good,
+  };
+
   render() {
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      h1 className = "App-title" > Welcome to React < /h1> < /
-      header > <
-      p className = "App-intro" >
-      To get started, edit < code > src / App.js < /code> and save to reload. < /
-      p > <
-      /div>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"> Welcome to React </h1>{' '}
+        </header>{' '}
+        <p className="App-intro">
+          To get started, edit <code> src / App.js </code> and save to reload.{' '}
+        </p>{' '}
+      </div>
     );
   }
 }
